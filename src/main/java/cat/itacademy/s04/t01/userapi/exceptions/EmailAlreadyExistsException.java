f681df1c9cb659dc1011a3639c0295980661459c
ccasro <ccasro@gmail.com>
2026-01-17 12:47:28 +0100

    feat: add EmailAlreadyExistsException

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s04.t01.userapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("User with email " + email + " already exists.");
+    }
+}
