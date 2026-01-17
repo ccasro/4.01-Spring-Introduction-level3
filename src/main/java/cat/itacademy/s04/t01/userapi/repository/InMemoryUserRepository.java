4c60c31d84c02781fa3cd3c47cf95ec2bc6b530d
ccasro <ccasro@gmail.com>
2026-01-17 10:35:18 +0100

    feat: add Repository annotation

--- src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -1,12 +1,14 @@
 package cat.itacademy.s04.t01.userapi.repository;
 
 import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public class InMemoryUserRepository implements UserRepository {
 
     private final List<User> users = new ArrayList<>();
